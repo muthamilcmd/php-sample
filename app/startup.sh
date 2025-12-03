96327850a55f14d04f2e68fde8a39781ddc4c12d
muthamilcmd <muthamilkumarans789@gmail.com>
2025-12-03 17:44:32 +0530

    Update startup.sh

--- app/startup.sh
+++ app/startup.sh
@@ -1,7 +1,9 @@
-#!/bin/bash
-  # Install Composer
-  curl -sS https://getcomposer.org/installer | php
-  mv composer.phar /usr/local/bin/composer
-  chmod +x /usr/local/bin/composer
-  cp /home/site/wwwroot/nginx_custom.conf /etc/nginx/sites-enabled/default
-  service nginx reload
+bash
+Copy
+#!/bin/bash  
+cd /home/site/wwwroot  
+php artisan config:clear  
+php artisan route:clear  
+php artisan view:clear  
+cp nginx_custom.conf /etc/nginx/sites-enabled/default  
+service nginx reload
