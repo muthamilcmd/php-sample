f83653639cbcfd4c3d203b1844500407775063a4
muthamilcmd <muthamilkumarans789@gmail.com>
2025-12-03 17:55:33 +0530

    Delete app/startup.sh

--- app/startup.sh
+++
@@ -1,9 +0,0 @@
-bash
-Copy
-#!/bin/bash  
-cd /home/site/wwwroot  
-php artisan config:clear  
-php artisan route:clear  
-php artisan view:clear  
-cp nginx_custom.conf /etc/nginx/sites-enabled/default  
-service nginx reload
