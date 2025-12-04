29ac9b1a1312d644c9c534bafac0103cd798dfec
muthamilcmd <muthamilkumarans789@gmail.com>
2025-12-04 07:41:57 +0530

    Update db.php

--- app/db.php
+++ app/db.php
@@ -1,19 +1,19 @@
 <?php
-$host = getenv('DB_HOST');
-$user = getenv('DB_USER');
-$pass = getenv('DB_PASS');
-$db   = getenv('DB_NAME');
-$port = getenv('DB_PORT') ?: 3306;
 
-// SSL CA certificate path
-$sslCA = __DIR__ . "/DigiCertGlobalRootG2.crt.pem";
+$host = getenv("DB_HOST");
+$user = getenv("DB_USER");
+$pass = getenv("DB_PASS");
+$dbname = getenv("DB_NAME");
+$port = getenv("DB_PORT");
 
 $conn = mysqli_init();
-mysqli_ssl_set($conn, NULL, NULL, $sslCA, NULL, NULL);
 
-if (!mysqli_real_connect($conn, $host, $user, $pass, $db, $port, MYSQLI_CLIENT_SSL)) {
-    error_log("MySQL Connection Error: " . mysqli_connect_error());
-    die("DB CONNECTION FAILED");
+// SSL required
+mysqli_ssl_set($conn, NULL, NULL, "DigiCertGlobalRootG2.crt.pem", NULL, NULL);
+
+if (!mysqli_real_connect($conn, $host, $user, $pass, $dbname, $port, MYSQLI_CLIENT_SSL)) {
+    die("DB ERROR: " . mysqli_connect_error());
 }
+
 ?>
 
